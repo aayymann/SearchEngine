485d56535dfa95255c2fc3cd302d368e18795f12
Ahmed Ayman <ahmed.ayman16@yahoo.com>
2021-05-19 21:31:35 +0200

    2 Concatenated URLS problem solved

--- Project/src/crawler/Crawler.java
+++ Project/src/crawler/Crawler.java
@@ -12,21 +12,20 @@ public class Crawler {
 
     public static void main(String[] args) {
         //
-        HashMap <String, ArrayList<String>> webRobots = new HashMap<String, ArrayList<String>>();
+        HashMap<String, ArrayList<String>> webRobots = new HashMap<String, ArrayList<String>>();
         //--File Paths
-        String seedsFilePath="./urlSeeds.txt";
-        String pcPath="./pc.txt";
+        String seedsFilePath = "./urlSeeds.txt";
+        String pcPath = "./pc.txt";
         //--Read URL seeds from InputFile
-        int compareWith=0;
+        int compareWith = 0;
         compareWith = ManipulateFile.ReadNumberFromFile(pcPath); // old value of pc counter
-        ManipulateFile.WriteNumberInFile(pcPath,compareWith);
-        System.out.println(compareWith);
+        ManipulateFile.WriteNumberInFile(pcPath, compareWith);
         //--If the end of file is reached or the number of crawls per crawl= 5000
-        while(ManipulateFile.ReadFile(seedsFilePath,compareWith)[0]!="" && ManipulateFile.ReadNumberFromFile(pcPath)<5000){
-            String webSiteURl =ManipulateFile.ReadFile(seedsFilePath,compareWith)[0];
-            compareWith=ManipulateFile.ReadNumberFromFile(pcPath); //read from the pc file
-            compareWith= Integer.parseInt(ManipulateFile.ReadFile(seedsFilePath,compareWith)[1]);  // increment it
-            ManipulateFile.WriteNumberInFile(pcPath,compareWith); // save it in the pc file
+        while (ManipulateFile.ReadFile(seedsFilePath, compareWith)[0] != "" && ManipulateFile.ReadNumberFromFile(pcPath) < 5000) {
+            String webSiteURl = ManipulateFile.ReadFile(seedsFilePath, compareWith)[0];
+            compareWith = ManipulateFile.ReadNumberFromFile(pcPath); //read from the pc file
+            compareWith = Integer.parseInt(ManipulateFile.ReadFile(seedsFilePath, compareWith)[1]);  // increment it
+            ManipulateFile.WriteNumberInFile(pcPath, compareWith); // save it in the pc file
             System.out.println(webSiteURl);
             //--Visit HomePage to generate the robots.txt map
             boolean isVisitable = true;
@@ -34,19 +33,19 @@ public class Crawler {
             try {
                 URLHandle = new URL(webSiteURl);
                 String robotsPage;
-                int length =URLHandle.getHost().split("\\.").length;
-                String mainDomainName = URLHandle.getHost().split("\\.")[length-2];
-                String TLD = URLHandle.getHost().split("\\.")[length-1];
+                int length = URLHandle.getHost().split("\\.").length;
+                String mainDomainName = URLHandle.getHost().split("\\.")[length - 2];
+                String TLD = URLHandle.getHost().split("\\.")[length - 1];
                 System.out.println(mainDomainName);
-                robotsPage= URLHandle.getProtocol() + "://www." + mainDomainName + "."+ TLD+ "/robots.txt";
+                robotsPage = URLHandle.getProtocol() + "://www." + mainDomainName + "." + TLD + "/robots.txt";
                 System.out.println("FOR THE ROBOTS " + robotsPage);
                 //--If the robots.txt of the homepage website already exists
                 if (webRobots.containsKey(mainDomainName)) {
                     //--Check before visiting if it is an allowable page
                     System.out.println("THE ROBOTS EXISTS");
                     ArrayList<String> disallowedPaths = webRobots.get(URLHandle.getHost());
-                    for (int i = 0 ; i< disallowedPaths.size() ; i++){
-                        if (URLHandle.getFile().matches(disallowedPaths.get(i).replace("*",".*"))){
+                    for (int i = 0; i < disallowedPaths.size(); i++) {
+                        if (URLHandle.getFile().matches(disallowedPaths.get(i).replace("*", ".*"))) {
                             isVisitable = false;
                             break;
                         }
@@ -55,37 +54,40 @@ public class Crawler {
                     //
                     System.out.println("THE ROBOTS DOESN'T EXIST");
                     URL robotsURL = new URL(robotsPage);
-                    String robotsHTML=URLHandling.OpenURLFn(robotsURL);
+                    String robotsHTML = URLHandling.OpenURLFn(robotsURL);
                     String mapKey = URLHandle.getHost();
-                    URLHandling.RobotsChecker(robotsHTML,webRobots,mapKey);
+                    URLHandling.RobotsChecker(robotsHTML, webRobots, mapKey);
                     //--Check before visiting if it is an allowable page
                     ArrayList<String> disallowedPaths = webRobots.get(mapKey);
-                    for (int i = 0 ; i< disallowedPaths.size() ; i++){
-                        if (URLHandle.getFile().matches(disallowedPaths.get(i).replace("*",".*"))){
+                    for (int i = 0; i < disallowedPaths.size(); i++) {
+                        if (URLHandle.getFile().matches(disallowedPaths.get(i).replace("*", ".*"))) {
                             isVisitable = false;
                             break;
                         }
                     }
                 }
-                if(isVisitable){
-                    //--TODO::Check if this website is not in the visitable file
-                    String gennHTMLString=URLHandling.OpenURLFn(URLHandle);
+                //--Check that the page is not in the Robots.txt
+                if (isVisitable) {
+                    String gennHTMLString = URLHandling.OpenURLFn(URLHandle);
                     //System.out.println(gennHTMLString);
+                    //--Get the hyperlinks
                     Pattern p = Pattern.compile("<a[^>]+href=\"(.*?)\"", Pattern.DOTALL);
                     Matcher m = p.matcher(gennHTMLString);
 
-                    while (m.find())
-                    {
-                        String tempURL=m.group(1);
-                        tempURL=tempURL.trim();
-                        //System.out.println(tempURL + "\t ORIGINAL");
-                       if(!m.group(1).contains(URLHandle.getProtocol()))
-                            tempURL=URLHandle.getProtocol()+"://"+URLHandle.getHost()+m.group(1).trim();
+                    while (m.find()) {
+                        String tempURL = m.group(1);
+                        tempURL = tempURL.trim();
+
+                        //--If the retrieved link does not contain the main sub domain (Concatenate it)
+                        if (!m.group(1).contains("http") || !m.group(1).startsWith("//"))
+                            tempURL = URLHandle.getProtocol() + "://" + URLHandle.getHost() + m.group(1).trim();
 
                         //System.out.println(tempURL);
 
-                        if(!ManipulateFile.IsStringPresent(seedsFilePath,tempURL)){ // if the extracted hyperlink is not present in the url seeds
-                            ManipulateFile.AppendOnFile(seedsFilePath,tempURL);
+                        //--If the extracted hyperlink is not present in the url seeds
+                        if (!ManipulateFile.IsStringPresent(seedsFilePath, tempURL)) {
+                            ManipulateFile.AppendOnFile(seedsFilePath, tempURL);
+                            //--TODO:: HAKAR
                         }
                     }
                     //System.out.println("This host is not on the robots page so we will visit");
--- Project/src/crawler/ManipulateFile.java
+++ Project/src/crawler/ManipulateFile.java
@@ -10,22 +10,22 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class ManipulateFile {
-    static  String [] ReadFile(String filePath,int compareWith) {
-        String websiteURL="";
-        int counter=0;
-        String [] arr = new String[2];
+    static String[] ReadFile(String filePath, int compareWith) {
+        String websiteURL = "";
+        int counter = 0;
+        String[] arr = new String[2];
         File filesToBeCrawled = new File(filePath);
         if (!filesToBeCrawled.exists()) {
             System.out.println("Seeds files don't exist!!!");
-            arr[0]="";
-            arr[1]="0";
+            arr[0] = "";
+            arr[1] = "0";
             return arr;
         }
         //
         try {
             Scanner myReader = new Scanner(filesToBeCrawled);
             while (myReader.hasNextLine()) {
-                if (compareWith == counter){
+                if (compareWith == counter) {
                     websiteURL = myReader.nextLine();
                     break;
                 }
@@ -33,54 +33,57 @@ public class ManipulateFile {
                 counter++;
             }
             compareWith = counter + 1;
-        } catch (FileNotFoundException ex) {}
-        arr[0]=websiteURL;
+        } catch (FileNotFoundException ex) {
+        }
+        arr[0] = websiteURL;
         arr[1] = String.valueOf(compareWith);
         return arr;
     }
-    static void WriteNumberInFile(String filePath,int pc){
-        try{
+
+    static void WriteNumberInFile(String filePath, int pc) {
+        try {
             FileWriter myWriter = new FileWriter(filePath);
             String pcString = String.valueOf(pc);
             myWriter.write(pcString);
             myWriter.close();
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Could not write in the program counter file");
         }
     }
-    static int ReadNumberFromFile (String filePath){
-        int pcCounter=-1;
-            try{
-                File pcFile = new File(filePath);
-                Scanner myReader = new Scanner(pcFile);
-                if(myReader.hasNextLine()){
-                    pcCounter=Integer.parseInt(myReader.nextLine());
-                }
+
+    static int ReadNumberFromFile(String filePath) {
+        int pcCounter = -1;
+        try {
+            File pcFile = new File(filePath);
+            Scanner myReader = new Scanner(pcFile);
+            if (myReader.hasNextLine()) {
+                pcCounter = Integer.parseInt(myReader.nextLine());
             }
-            catch(FileNotFoundException ex){}
+        } catch (FileNotFoundException ex) {
+        }
         return pcCounter;
     }
 
-    static boolean IsStringPresent(String filePath , String searchedForString){
+    static boolean IsStringPresent(String filePath, String searchedForString) {
         File filesToBeCrawled = new File(filePath);
         try {
-            String tempSearchedFor = searchedForString+"/";
+            String tempSearchedFor = searchedForString + "/";
             Scanner myReader = new Scanner(filesToBeCrawled);
             while (myReader.hasNextLine()) {
                 String readLine = myReader.nextLine();
-                if(readLine.equals(searchedForString) || readLine.equals(tempSearchedFor))
+                if (readLine.equals(searchedForString) || readLine.equals(tempSearchedFor))
                     return true;
             }
-        } catch (FileNotFoundException ex) {}
+        } catch (FileNotFoundException ex) {
+        }
         return false;
     }
 
-    static void AppendOnFile(String filePath, String toBeAddedString){
-        toBeAddedString+="\n";
+    static void AppendOnFile(String filePath, String toBeAddedString) {
+        toBeAddedString += "\n";
         try {
             Files.write(Paths.get(filePath), toBeAddedString.getBytes(), StandardOpenOption.APPEND);
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("FAILURE TO APPEND ON FILE");
         }
     }
