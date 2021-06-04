0003c02b572491e5edfdec95647ebc3427cdbb70
Ghallab <ghallab98@gmail.com>
2021-06-04 10:43:23 +0200

    Connecting Database and primary commands

--- Project/src/indexer/Database.java
+++ Project/src/indexer/Database.java
@@ -1,66 +1,74 @@
 package indexer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.DatabaseMetaData;
-import java.sql.Statement;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoCredential;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Database {
-    public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:Test.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
+    public static MongoDatabase database;
+    public static void createDatabase() {
 
-            System.out.println("Connection to SQLite has been established.");
+        //Remove intensive logging
+        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+        mongoLogger.setLevel(Level.SEVERE);
 
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
+        //Connecting to Database with local host
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        database = mongoClient.getDatabase("index");
+        //Drop the collection first because you can't create a new one with same name
+        dropCollection("Words"); //TODO: Must Be Removed when code is Working properly its for test purposes only
+        //Creating a collection This needs to Run Once only
+        database.createCollection("Words"); //TODO: Must Be Removed when code is Working properly its for test purposes only
     }
-    public static void createNewDatabase(String fileName) {
 
-        String url = "jdbc:sqlite:E:\\UNIVERSITY\\Semester 8\\Advanced Programming Techniques\\Project\\SearchEngine\\Project\\src\\indexer\\" + fileName;
-
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+    public static void dropCollection(String CollectionName){
+        //Getting a Collection
+        //MongoCollection<Document> collection = database.getCollection("sampleCollection");
+        database.getCollection(CollectionName).drop();
     }
-    public static void createNewTable() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:E:\\UNIVERSITY\\Semester 8\\Advanced Programming Techniques\\Project\\SearchEngine\\Project\\src\\indexer\\test.db";
+    public static void insertDoc(Document word){
+        database.getCollection("Words").insertOne(word);
+    }
 
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS words (\n"
-                + "	words text NOT NULL UNIQUE,\n"
-                + "	TF integer NOT NULL,\n"
-                + "	PRIMARY KEY(words)\n"
-                + ");";
-
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+    public static void insertManyDocs(Document[] words){
+        /*      Creating a document
+        Document document1 = new Document("title", "newDB")
+                .append("description", "database")
+                .append("likes", 100)
+                .append("url", "http://www.tutorialspoint.com/mongodb/")
+                .append("by", "tutorials point");
+        */
+        List<Document> list = new ArrayList<Document>();
+        for(int i =0;i<words.length;i++) {
+            list.add(words[i]);
+        }
+        database.getCollection("Words").insertMany(list);
+    }
+    public static void printDatabase(){
+        // Retrieving a collection
+        MongoCollection<Document> collection = database.getCollection("Words");
+        // Getting the iterable object
+        FindIterable<Document> iterDoc = collection.find();
+        int i = 1;
+        // Getting the iterator
+        Iterator it = iterDoc.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+            i++;
         }
     }
 }
